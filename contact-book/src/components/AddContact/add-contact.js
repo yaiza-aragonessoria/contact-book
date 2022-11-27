02112372b67b5adc27a18f48d78f31ad7708bd06
Yaiza Aragonés Soria <yaiza.aragonessoria@gmail.com>
2022-11-27 15:13:05 +0100

    better styling

--- contact-book/src/components/AddContact/add-contact.js
+++ contact-book/src/components/AddContact/add-contact.js
@@ -5,45 +5,43 @@ const AddContact = ({name, address, species, favColor, isEditing, handleSubmit, 
         <>
         <form className="form" onSubmit={handleSubmit}>
             <div className="input-container">
-                <div className="name-container">
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" 
-                            id="name" 
-                            name="name" 
-                            value={name}
-                            onChange={e => handleContactDetails(e)}>
-                    </input>
-                </div>
-                <div className="address-container">
-                    <label htmlFor="address">Address:</label>
-                    <input type="text" 
-                            id="address" 
-                            name="address" 
-                            value={address}
-                            onChange={e => handleContactDetails(e)}>
-                    </input>
-                </div>
-                <div className="species-container">
-                    <label htmlFor="species">Species:</label>
-                    <input type="text" 
-                            id="species" 
-                            name="species" 
-                            value={species}
-                            onChange={e => handleContactDetails(e)}>
-                    </input>
-                </div>
-                <div className="favColor-container">
-                    <label htmlFor="favColor">Fav Color:</label>
-                    <input type="text" 
-                            id="favColor" 
-                            name="favColor" 
-                            value={favColor}
-                            onChange={e => handleContactDetails(e)}>
-                    </input>
-                </div>
+                <label htmlFor="name">Name:</label>
+                <input type="text" 
+                        id="name" 
+                        name="name" 
+                        value={name}
+                        onChange={e => handleContactDetails(e)}>
+                </input>
+            </div>
+            <div className="input-container">
+                <label htmlFor="address">Address:</label>
+                <input type="text" 
+                        id="address" 
+                        name="address" 
+                        value={address}
+                        onChange={e => handleContactDetails(e)}>
+                </input>
+            </div>
+            <div className="input-container">
+                <label htmlFor="species">Species:</label>
+                <input type="text" 
+                        id="species" 
+                        name="species" 
+                        value={species}
+                        onChange={e => handleContactDetails(e)}>
+                </input>
+            </div>
+            <div className="input-container">
+                <label htmlFor="favColor">Fav Color:</label>
+                <input type="text" 
+                        id="favColor" 
+                        name="favColor" 
+                        value={favColor}
+                        onChange={e => handleContactDetails(e)}>
+                </input>
             </div>
             <div className="avatar-container">
-                <label htmlFor="avatar">Upload avatar</label>
+                <label className="avatar-label" htmlFor="avatar">Upload avatar</label>
                 <input
                 type="file"
                 name="avatar"
